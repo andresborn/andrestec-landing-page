4ab7758f11a4a94f453bcad5f72d685fd318f595
andresborn <andres.born@gmail.com>
2021-02-03 17:01:50 -0600

    Added css animations to letters

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,16 +4,16 @@ export const Header = () => {
     return (
         
             <div className="d-flex justify-content-center justify-content-sm-end mx-5 header">
-                <span className="nav-title-letter">A</span>
-                <span className="nav-title-letter">n</span>
-                <span className="nav-title-letter">d</span>
-                <span className="nav-title-letter">r</span>
-                <span className="nav-title-letter">e</span>
-                <span className="nav-title-letter">s</span>
+                <span className="nav-title-letter bounce">A</span>
+                <span className="nav-title-letter bounce">n</span>
+                <span className="nav-title-letter bounce">d</span>
+                <span className="nav-title-letter bounce">r</span>
+                <span className="nav-title-letter bounce">e</span>
+                <span className="nav-title-letter bounce">s</span>
                 <span>&nbsp;</span>
-                <span className="nav-title-letter">T</span>
-                <span className="nav-title-letter">e</span>
-                <span className="nav-title-letter">c</span>
+                <span className="nav-title-letter bounce">T</span>
+                <span className="nav-title-letter bounce">e</span>
+                <span className="nav-title-letter bounce">c</span>
             </div>
     )
 }
