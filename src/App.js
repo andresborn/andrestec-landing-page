22564246daa581c6e4943216fd9c33bc566f1267
andresborn <andres.born@gmail.com>
2021-02-07 12:29:47 -0600

    Added bgimg to About, change bgcolor to body

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { Navbar } from './components/Navbar.js';
 import { Header } from './components/Header.js';
+import About from './components/About.js';
 
 const App = () => {
   return (
    <div className="App">
      <Navbar />
      <Header />
+     <About />
    </div>
   );
 }
---
+++ src/components/About.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const About = props => {
+    return (
+        <div className="d-flex justify-content-center">
+            <div className="about-background">
+                <h1>Hi</h1>
+                <p>Lorem10</p>
+            </div>
+        </div>
+    )
+}
+
+About.propTypes = {
+
+}
+
+export default About
